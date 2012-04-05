1ee2783fce395905d177633cb922b151ea3d726e
David Olivier <david.olivier@tech.geocent.com>
2012-04-05 12:58:20 -0500

    deleted commented out code

--- selenium_tests/src/com/geocent/opencop/tests/OpenCopTest.java
+++ selenium_tests/src/com/geocent/opencop/tests/OpenCopTest.java
@@ -31,16 +31,6 @@ public class OpenCopTest {
 	public void testOpenCopExists() throws Exception {
 		driver.get("http://localhost/opencop");
 		assertEquals("OpenCOP 2.0", driver.getTitle());
-//		driver.findElement(By.id("ext-gen26")).click();
-//		for (int second = 0;; second++) {
-//			if (second >= 60) fail("timeout");
-//			try { if (isElementPresent(By.cssSelector("div.x-grid3-cell-inner.x-grid3-col-0"))) break; } catch (Exception e) {}
-//			Thread.sleep(1000);
-//		}
-//
-//		// ERROR: Caught exception [ERROR: Unsupported command [doubleClickAt]]
-//		driver.findElement(By.id("ext-gen181")).click();
-//		assertEquals("example", driver.findElement(By.id("extdd-24")).getText());
 	}
 
 	@After
@@ -51,15 +41,4 @@ public class OpenCopTest {
 			fail(verificationErrorString);
 		}
 	}
-	
-	// Private methods:
-
-//	private boolean isElementPresent(By by) {
-//		try {
-//			driver.findElement(by);
-//			return true;
-//		} catch (NoSuchElementException e) {
-//			return false;
-//		}
-//	}
 }
