9b8f92f22eeb477bf2cc60f470ed218754a0434c
Stephen Mann <thanthese@gmail.com>
2012-05-29 07:13:40 -0500

    Improved startWatch comments.

--- scripts/startWatch.sh
+++ scripts/startWatch.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 # Modify `updated` in tomcat if dev files change.
+#
+# If using tomcat, it's recommended to make the following change so that
+# changes to `updated` show immediately. In `/etc/tomcat6/context.xml`,
+# add `cachingAllowed="false"` to the `<Context>` element.
 
 watch -n 0.1 'find ~/OpenCOP/web_app/ -type f | grep -v "/images/\|/lib/" | xargs stat | grep "Change\|File" > /var/lib/tomcat6/webapps/opencop-updated/updated'
