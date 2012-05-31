40dd2198b5fe7fd9498b28d4fad8be62263e6bc4
Stephen Mann <thanthese@gmail.com>
2012-05-31 13:16:00 -0500

    Even moar!

--- scripts/install.sh
+++ scripts/install.sh
@@ -220,8 +220,8 @@ su $username -c 'cd ~/OpenCOP/db-setup; sh ./populate-db'
 ## TODO: Check the return values from these calls
 for namespace in "opencop"
 do
-  echo -n "$p Adding $1 workspace..."
-  response=`curl -s -u admin:geoserver --write-out %{http_code} -XPOST -H 'Content-type: text/xml' -d '<namespace><uri>http://opencop.geocent.com</uri><prefix>$1</prefix></namespace>' http://$localip/geoserver/rest/namespaces`
+  echo -n "$p Adding $namespace workspace..."
+  response=`curl -s -u admin:geoserver --write-out %{http_code} -XPOST -H 'Content-type: text/xml' -d '<namespace><uri>http://opencop.geocent.com</uri><prefix>$namespace</prefix></namespace>' http://$localip/geoserver/rest/namespaces`
   if [ "$response" = "201" ]; then
       echo "Success."
   else
@@ -234,7 +234,7 @@ for store in "opencop" "dynamic_feature"
 do
   echo -n "$p Adding the $store data store..."
   ## TODO: Not all the params are specified here(spaces in, for example, "max connections", hose up the xml). Doesn't appear they need to be, but...
-  response=`curl -s -u admin:geoserver --write-out %{http_code} -XPOST -H 'Content-type: text/xml' -d "<dataStore><name>$store</name><connectionParameters><host>$localip</host><port>5432</port><database>$store</database><user>opencop</user><dbtype>postgis</dbtype><passwd>57levelsofeoc</passwd></connectionParameters></dataStore>" http://$localip/geoserver/rest/workspaces/$1/datastores`
+  response=`curl -s -u admin:geoserver --write-out %{http_code} -XPOST -H 'Content-type: text/xml' -d "<dataStore><name>$store</name><connectionParameters><host>$localip</host><port>5432</port><database>$store</database><user>opencop</user><dbtype>postgis</dbtype><passwd>57levelsofeoc</passwd></connectionParameters></dataStore>" http://$localip/geoserver/rest/workspaces/$store/datastores`
   if [ "$response" = "201" ]; then
       echo "Success."
   else
