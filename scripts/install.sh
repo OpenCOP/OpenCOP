24543fd053bf7b5b3c167714d578045669d14277
Bryan Joseph <bryanjos@gmail.com>
2013-03-15 14:59:18 -0500

    updated deploy and install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,72 +1,33 @@
 #!/bin/sh
 
-# constants
 p="\n[OpenCOP Install] "
 
-# Check that the script is being run as root
-if [ $(id -u) -ne 0 ]; then
-	echo "Please use 'sudo' to run this script."
-	exit 2
-fi
+cd ~
 
-confirmed="n"
-while [ "x$confirmed" != "xy" ]
-do
+echo "$p Installing OpenGeo-Suite"
+wget -qO- http://apt.opengeo.org/gpg.key | sudo apt-key add -
+sudo su -c "echo 'deb http://apt.opengeo.org/suite/v3/ubuntu lucid main' >> /etc/apt/sources.list"
+sudo apt-get update
+sudo apt-get install -y opengeo-suite
 
-# Get the shell user to use
-read -p "Enter the username for the account under which to install OpenCOP: " username
-
-# Get shell user's password
-passgood="no"
-stty -echo
-while [ $passgood = "no" ]
-do
-	read -p "Enter that user's password: " password; echo
-	read -p "Enter that user's password again: " password2; echo
-	if [ "$password" = "$password2" ]; then
-		passgood="yes"
-	else
-		echo "Passwords don't match. Try again."
-	fi
-done
-stty echo
-
-# Get the IP address to install everything on
-read -p "Enter the IP address to use for GeoServer/OpenCOP/postgres (Hit ENTER to use localhost): " localip
-
-if [ "x$localip" = "x" ]; then
-    localip="127.0.0.1"
-fi
-
-echo "You have entered:\n  Username: $username\n  IP: $localip"
-read -p "Is this correct? (y/n): " confirmed
-
-done
-
-echo "$p Away we go..."
-
-# vi for the masses!
-if grep -q "set -o vi" ~/.bashrc
+# install git
+if git --version
 then
-  echo "$p vi blissfully already configured"
+  echo "$p git already installed"
 else
-  echo "$p configuring vi (we are, afterall, civilized)"
-  set -o vi
-  sed -ie '$a\\nset -o vi' ~/.bashrc
+  echo "$p install git"
+  sudo apt-get install -y git-core
 fi
 
-
-# Install opengeo-suite
-if grep opengeo /etc/apt/sources.list
+# Install curl to use for REST calls to geoserver
+if curl --help
 then
-  echo "$p opengeo-suite is already installed"
+  echo "$p curl already installed"
 else
-  echo "$p Installing open-geo suite"
-  wget -qO- http://apt.opengeo.org/gpg.key | sudo apt-key add -
-  sudo sh -c "echo \"deb http://apt.opengeo.org/ubuntu lucid main\" >> /etc/apt/sources.list"
-  sudo apt-get update
-  sudo apt-get install -y opengeo-suite
+  echo "$p Installing curl"
+  apt-get install -y curl
 fi
+
 
 # Install apache2 and configure proxy
 if apache2 -v
@@ -79,7 +40,7 @@ else
   sudo sed -ie 'N;N;N;s/ *<!-- *\n* *\(<Connector.*port="8009".*>\) *\n* *-->/    \1/g' /etc/tomcat6/server.xml
   sudo a2enmod proxy_ajp
   # add proxypass rules to /etc/apache2/sites-enabled/000-default
-  sudo sed -ie "/<\/VirtualHost>/i\<Proxy *>    \n  Allow from 127.0.0.1    \n</Proxy>    \nProxyPass / ajp://$localip:8009/    \nProxyPassReverse / ajp://$localip:8009/    \n" /etc/apache2/sites-enabled/000-default
+  sudo sed -ie "/<\/VirtualHost>/i\<Proxy *>    \n  Allow from 127.0.0.1    \n</Proxy>    \nProxyPass / ajp://localhost:8009/    \nProxyPassReverse / ajp://localhost:8009/    \n" /etc/apache2/sites-enabled/000-default
   sudo service apache2 restart
 fi
 
@@ -94,132 +55,50 @@ else
   sudo sed -ie '/^JAVA_OPTS=/s/"$/ -Djava.awt.headless=true -Xms1024m -Xmx1024m -Xrs -XX:PerfDataSamplingInterval=500 -XX:MaxPermSize=256m"/' /etc/default/tomcat6
 fi
 
-# Create build user
-if id $username > /dev/null
-then
-	echo "$p User $username already exists"
-else
-	echo "$p Creating user $username"
-	# Encrypt the password so we can pass it to useradd -p
-	pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-	useradd -m -p $pass -s /bin/bash $username
-	if [ $? -eq 0 ]; then
-		echo "User $username has been added to system"
-	else
-		"Failed to add user $username"
-		exit 1
-	fi
-fi
+# install OpenCOP
+echo "$p Getting and Building OpenCOP"
+git clone git://github.com/OpenCOP/OpenCOP.git
+mkdir OpenCOP/build
+cd OpenCOP/src/main/webapp
+jar cvf ../../../build/opencop.war .
+sudo cp ~/OpenCOP/build/opencop.war /var/lib/tomcat6/webapps
 
-# give build user sudo access
-if sudo grep -q $username /etc/sudoers
-then
-  echo "$p $username already has sudo rights"
-else
-  echo "$p give $username sudo rights"
-  echo "$username   ALL=(ALL)       ALL" | sudo tee -a /etc/sudoers > /dev/null
-fi
-
-# install git
-if git --version
-then
-  echo "$p git already installed"
-else
-  echo "$p install git"
-  sudo apt-get install -y git-core
-fi
-
-# pull most recent source
-if test -s /home/$username/OpenCOP/.git/
-then
-  echo "$p Pulling latest OpenCOP source code from github"
-  sudo su $username -c 'cd ~/OpenCOP; git pull'
-else
-  echo "$p Downloading OpenCOP source code from github"
-  sudo su $username -c 'cd; git clone git://github.com/OpenCOP/OpenCOP.git'
-fi
-
-# setup postgres users
-if psql -U postgres -c '\du' | grep -q opencop
-then
-  echo "$p posgres users already exist"
-else
-  echo "$p create postgres users"
-  sudo sed -ie '/postgres.*ident/s/ident/trust/' /etc/postgresql/8.4/main/pg_hba.conf
-  sudo sed -ie '/local.*ident/s/ident/trust/' /etc/postgresql/8.4/main/pg_hba.conf
-  sudo service postgresql-8.4 restart
-  psql -U postgres -c "CREATE ROLE opencop LOGIN ENCRYPTED PASSWORD '57levelsofeoc' INHERIT CREATEDB CREATEROLE;"
-  psql -U postgres -c "CREATE ROLE fmanager LOGIN ENCRYPTED PASSWORD 'md5247826c7fcf1dfcb0fe1b350cd0cda2e' INHERIT CREATEDB CREATEROLE;"
-fi
-
-# create databases (and postgis-ify them)
-if psql -U postgres -c "\l" | grep -q opencop
-then
-  echo "$p necessary databases already exist"
-else
-  echo "$p create databases"
-  psql -U postgres -c "CREATE DATABASE opencop WITH OWNER = opencop CONNECTION LIMIT = -1;"
-  psql -U postgres -c "CREATE DATABASE dynamic_feature WITH ENCODING='UTF8' OWNER=fmanager TEMPLATE=template_postgis CONNECTION LIMIT=-1;"
-
-  echo "$p postgis-ify databases"
-  createlang plpgsql -U postgres dynamic_feature
-  psql -U postgres -f /usr/share/postgresql/8.4/contrib/postgis-1.5/postgis.sql -d dynamic_feature
-fi
-
-
-# Run the local_deploy script
-## TODO: Or run this as tomcat6 instead (if that works)
-echo "$p Running local_deploy script"
-su $username -c 'cd ~/OpenCOP; sh ./local_deploy'
-
-# Kinda hacky: The war copy will fail in local_deploy because the user doesn't have permissions to the tomcat directory
-# So copy it here
-cp /home/$username/OpenCOP/build/opencop.war /var/lib/tomcat6/webapps
+cd ~
 
 # Restart apache and tomcat
 sudo service apache2 restart
 sudo service tomcat6 restart
 
 
-# TODO: Will need to move the icons folder from OC1 to OC2 and do this again
-# move icons folder
-#echo "$p move icons folder from source to tomcat"
-#sudo cp -ur /home/$username/OpenCOP/opencop-icons/ /var/lib/tomcat6/webapps/
+# setup postgres users
+# Opengeo creates a postgres user named 'opengeo' with password 'opengeo'
+sudo sed -ie '/postgres.*ident/s/ident/trust/' /etc/postgresql/9.2/main/pg_hba.conf
+sudo sed -ie '/local.*ident/s/ident/trust/' /etc/postgresql/9.2/main/pg_hba.conf
+sudo service postgresql restart
 
+echo "Opengeo creates a postgres user named 'opengeo' with password 'opengeo' if asked to provide password"
+sudo echo "*:*:*:opengeo:opengeo" > .pgpass
+sudo chmod 0600 .pgpass
 
-# Install curl to use for REST calls to geoserver
-echo "$p Installing curl"
-apt-get install curl
+psql -U opengeo -c "CREATE ROLE opencop LOGIN ENCRYPTED PASSWORD '57levelsofeoc' INHERIT CREATEDB CREATEROLE;" -d postgres
+psql -U opengeo -c "CREATE ROLE fmanager LOGIN ENCRYPTED PASSWORD 'md5247826c7fcf1dfcb0fe1b350cd0cda2e' INHERIT CREATEDB CREATEROLE;" -d postgres
 
-# Wait for Geoserver to come up
-geoserver_up="false"
-echo -n "$p Waiting for Geoserver to come up. This may take a minute or two..."
+psql -U opengeo -c "CREATE DATABASE opencop WITH OWNER = opencop CONNECTION LIMIT = -1;" -d postgres
+psql -U opengeo -c "CREATE DATABASE dynamic_feature WITH ENCODING='UTF8' OWNER=fmanager TEMPLATE=template_postgis CONNECTION LIMIT=-1;" -d postgres
 
-while [ $geoserver_up != "true" ]
-do
-	response=`curl -s -XGET -u admin:geoserver -H 'Accept: text/xml' http://$localip/geoserver/rest/workspaces/`
+echo "$p postgis-ify databases"
+createlang plpgsql -U opengeo dynamic_feature
+psql -U opengeo -f /usr/share/postgresql/9.2/contrib/postgis-2.0/postgis.sql -d dynamic_feature
 
-	greppage=`echo $response | grep workspace`
-
-	if [ "x" != "x$greppage" ]; then
-		echo "Done."
-		geoserver_up="true"
-	else
-		echo -n "."
-		sleep 3
-	fi
-done
-
-
-# Populate the DB
-echo "$p Running populate-db script"
-su $username -c 'cd ~/OpenCOP/db-setup; sh ./populate-db'
+psql -U opengeo opencop -f OpenCOP/db-setup/icon.sql
+psql -U opengeo opencop -f OpenCOP/db-setup/hswg_icons.sql  # relies on icon.sql having run
+psql -U opengeo opencop -f OpenCOP/db-setup/populate-db.sql
 
 
 # Add the opencop workspace/namespace
 ## TODO: Check the return values from these calls
 echo -n "$p Adding opencop workspace..."
-response=`curl -s -u admin:geoserver --write-out %{http_code} -XPOST -H 'Content-type: text/xml' -d '<namespace><uri>http://opencop.geocent.com</uri><prefix>opencop</prefix></namespace>' http://$localip/geoserver/rest/namespaces`
+response=`curl -s -u admin:geoserver --write-out %{http_code} -XPOST -H 'Content-type: text/xml' -d '<namespace><uri>http://opencop.geocent.com</uri><prefix>opencop</prefix></namespace>' http://localhost:8080/geoserver/rest/namespaces`
 if [ "$response" = "201" ]; then
     echo "Success."
 else
@@ -227,20 +106,22 @@ else
     exit 1
 fi
 
+
 echo -n "$p Adding the opencop data store..."
 ## TODO: Not all the params are specified here(spaces in, for example, "max connections", hose up the xml). Doesn't appear they need to be, but...
-response=`curl -s -u admin:geoserver --write-out %{http_code} -XPOST -H 'Content-type: text/xml' -d "<dataStore><name>opencop</name><connectionParameters><host>$localip</host><port>5432</port><database>opencop</database><user>opencop</user><dbtype>postgis</dbtype><passwd>57levelsofeoc</passwd></connectionParameters></dataStore>" http://$localip/geoserver/rest/workspaces/opencop/datastores`
+response=`curl -s -u admin:geoserver --write-out %{http_code} -XPOST -H 'Content-type: text/xml' -d "<dataStore><name>opencop</name><enabled>true</enabled><connectionParameters><host>localhost</host><port>5432</port><database>opencop</database><user>opengeo</user><dbtype>postgis</dbtype><passwd>opengeo</passwd></connectionParameters></dataStore>" http://localhost:8080/geoserver/rest/workspaces/opencop/datastores`
 if [ "$response" = "201" ]; then
     echo "Success."
 else
-    echo "Failed. $response"
+    echo "Failedcd . $response"
     exit 1
 fi
+
 
 for layername in "baselayer" "config" "icon" "iconmaster" "iconstolayers" "layer" "layergroup"
 do
 echo -n "$p Publishing $layername layer... "
-response=`curl -s -u admin:geoserver --write-out %{http_code} -XPOST -H 'Content-type: text/xml' -d "<featureType><name>$layername</name><srs>EPSG:4326</srs><nativeBoundingBox><minx>0</minx><miny>0</miny><maxx>-1</maxx><maxy>-1</maxy></nativeBoundingBox><latLonBoundingBox><minx>-1</minx><miny>-1</miny><maxx>0</maxx><maxy>0</maxy></latLonBoundingBox></featureType>" http://$localip/geoserver/rest/workspaces/opencop/datastores/opencop/featuretypes`
+response=`curl -s -u admin:geoserver --write-out %{http_code} -XPOST -H 'Content-type: text/xml' -d "<featureType><name>$layername</name><srs>EPSG:4326</srs><nativeBoundingBox><minx>0</minx><miny>0</miny><maxx>-1</maxx><maxy>-1</maxy></nativeBoundingBox><latLonBoundingBox><minx>-1</minx><miny>-1</miny><maxx>0</maxx><maxy>0</maxy></latLonBoundingBox></featureType>" http://localhost:8080/geoserver/rest/workspaces/opencop/datastores/opencop/featuretypes`
 if [ "$response" = "201" ]; then
     echo "Success."
 else
@@ -248,6 +129,5 @@ else
     exit 1
 fi
 done
-
 
 echo "$p OpenCOP install complete. Have a nice day."
